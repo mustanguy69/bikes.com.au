6b29e159d875d04c052b1e0b2a877e59e7fe0bbb
Tanguy BELIN <tanguybelin@hotmail.com>
2019-12-04 11:19:13 +1100

    Final xmas popup

--- skin/frontend/tv_bikeworld_package/tv_bikeworld2/js/popup.js
+++ skin/frontend/tv_bikeworld_package/tv_bikeworld2/js/popup.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function(){
-    var popup = "<div class='popup-overlay col-sm-12'><div class='popup-content col-sm-4'><i class='fa fa-times-circle-o fa-3x close-popup'></i> </div> </div>";
-    var content = '<div class="col-sm-12">\n' +
+    var popup = "<div class='popup-overlay col-sm-12'><div class='popup-content col-sm-11 col-md-8 col-lg-6 col-xl-5'><i class='fa fa-times-circle-o fa-3x close-popup'></i> </div> </div>";
+    var content = '<div class="col-sm-9 col-sm-offset-3">\n' +
         '<div class="maps">&nbsp;</div>\n' +
         '<div class="maps" style="text-align: center;"><span style="font-size: x-large;"><strong>FOR DELIVERY BEFORE CHRISTMAS <br> WEB ORDERS MUST BE PLACED BY :</strong></span><br><br></div>\n' +
-        '<div class="maps" style="text-align: center;"><span style="font-size: x-medium;"><strong>- PERTH 15TH DEC</strong></span></div>\n' +
+        '<div class="maps" style="text-align: center;"><span style="font-size: x-medium;"><br><strong>- PERTH 15TH DEC</strong></span></div>\n' +
         '<div class="maps" style="text-align: center;"><span style="font-size: x-medium;"><strong>- BRISBANE 16TH DEC</strong></span></div>\n' +
         '<div class="maps" style="text-align: center;"><span style="font-size: x-medium;"><strong>- MELBOURNE / ADELAIDE / SYDNEY 19TH DEC</strong></span></div>\n' +
         '<div class="maps" style="text-align: center;"><span style="font-size: x-medium;"><strong>- REGIONAL AREAS - ADD AT LEAST 1 WEEK TO CAPITAL CITY DATES FOR DELIVERY BEFORE CHRISTMAS.</strong></span></div>\n';
+
     jQuery( window ).load(function() {
         jQuery('.cms-home2 .wrapper').prepend(popup);
         jQuery('.popup-content').append(content);
