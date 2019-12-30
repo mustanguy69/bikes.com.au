0368d4af26b89a1120c3f2e5eac5f0c22257ab68
Tanguy BELIN <tanguybelin@hotmail.com>
2019-12-31 10:31:01 +1100

    Add yellozw banner

--- skin/frontend/tv_bikeworld_package/tv_bikeworld2/js/popup.js
+++ skin/frontend/tv_bikeworld_package/tv_bikeworld2/js/popup.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function(){
     var popup = "<div class='popup-overlay col-sm-12'><div class='popup-content col-sm-11 col-md-8 col-lg-6 col-xl-5'><i class='fa fa-times-circle-o fa-3x close-popup'></i> </div> </div>";
-    var content = '<div class="col-sm-9 col-sm-offset-3">\n' +
+    var content = '<div class="col-sm-9">\n' +
         '<div class="maps">&nbsp;</div>\n' +
-        '<div class="maps" ><span style="font-size: 43px;">ALL <br> <strong>SUMMER <br> LONG</strong></span><br><br></div>\n' +
-        '<div class="maps"><span style="font-size: 20px;"><br><br>MASSIVE SAVINGS ON ALL SCOTT & FOCUS BIKES .</span></div>';
+        '<div class="maps" ><span style="font-size: 43px;">ALL <br> <strong>SUMMER <br> LONG</strong></span><br><br><br></div></div>\n' +
+        '<div class="col-sm-12" style="margin:0;background-color: rgba(249, 246, 9,0.5);padding-top: 15px;padding-bottom: 8px">\n' +
+        '<div class="maps"><span style="font-size: 25px;color: black;">MASSIVE SAVINGS ON ALL SCOTT & FOCUS BIKES .</span></div>'+
+        '</div>';
 
     jQuery( window ).load(function() {
         jQuery('.cms-home2 .wrapper').prepend(popup);
