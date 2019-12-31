614e909d5e6fe9a20e613df9fa31f2822405436c
Tanguy BELIN <tanguybelin@hotmail.com>
2019-12-31 11:01:13 +1100

    increae text

--- skin/frontend/tv_bikeworld_package/tv_bikeworld2/js/popup.js
+++ skin/frontend/tv_bikeworld_package/tv_bikeworld2/js/popup.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function(){
-    var popup = "<div class='popup-overlay col-sm-12'><div class='popup-content col-sm-11 col-md-8 col-lg-6 col-xl-5'><i class='fa fa-times-circle-o fa-3x close-popup'></i> </div> </div>";
+    var popup = "<div class='popup-overlay col-sm-12'><div class='popup-content col-sm-11 col-md-7 col-lg-6 col-xl-6'><i class='fa fa-times-circle-o fa-3x close-popup'></i> </div> </div>";
     var content = '<div class="col-sm-9">\n' +
         '<div class="maps">&nbsp;</div>\n' +
         '<div class="maps" ><span style="font-size: 43px;">ALL <br> <strong>SUMMER <br> LONG</strong></span><br><br><br></div></div>\n' +
-        '<div class="col-sm-12" style="margin-left:0;background-color: rgba(249, 246, 9,0.7);padding-top: 15px;padding-bottom: 8px">\n' +
-        '<div class="maps"><span style="font-size: 22px; color: red;padding-left: 15px; font-weight: bold" class="text-yellow">MASSIVE SAVINGS ON ALL SCOTT & FOCUS BIKES .</span></div>'+
+        '<div class="col-sm-12" style="margin-left:0;background-color: rgba(249, 246, 9,0.9);padding-top: 15px;padding-bottom: 8px">\n' +
+        '<div class="maps"><span style="font-size: 1.7vw; color: red;padding-left: 15px; font-weight: bold" class="text-yellow">MASSIVE SAVINGS ON ALL SCOTT & FOCUS BIKES .</span></div>'+
         '</div>';
 
     jQuery( window ).load(function() {
